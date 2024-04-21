26ffa5f697ce3e22345d6fa73cfd7abeda647044
m-shiroi <maho.shiroi@gmail.com>
2024-04-21 17:16:46 +0900

    rubocop

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -5,8 +5,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "http://localhost:8002"
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
   end
 end
