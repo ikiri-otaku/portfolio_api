f5051c82fee83dd059bf60ea06c63981465bb4bf
yuita-yoshihiko <soup998211@gmail.com>
2024-04-23 23:16:27 +0900

    マイグレーションファイルの作成と実行

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_135034) do
+  create_table "organizations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.string "github_username", limit: 50
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["github_username"], name: "index_organizations_on_github_username", unique: true
+  end
+
 end
