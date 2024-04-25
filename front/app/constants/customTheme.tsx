f09984fe1077be2985a25ed9d2834459710079e2
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-04-25 22:34:20 +0900

    Add: bg-colorを基準としたカスタムカラー追加

--- front/app/constants/customTheme.tsx
+++ front/app/constants/customTheme.tsx
@@ -1,30 +1,42 @@
-import { Button, createTheme, MantineProvider, rem } from '@mantine/core';
+import { Button, createTheme, MantineProvider, rem } from "@mantine/core";
 
 const theme = createTheme({
   colors: {
     // Add your color
+    darkGray: [
+      "#f5f5f5",
+      "#e7e7e7",  // bg_color
+      "#cdcdcd",
+      "#b2b2b2",
+      "#9a9a9a",
+      "#8b8b8b",
+      "#848484",
+      "#717171",
+      "#656565",
+      "#575757",
+    ],
     violet: [
       "#f6ecff",
       "#e7d6fb",
       "#caabf1",
-      "#ac7ce8", 
+      "#ac7ce8",
       "#915BD6", //right_color
       "#833cdb",
       "#7b2eda",
       "#632BAD", //main_color
       "#5d1cae",
-      "#501599"
+      "#501599",
     ],
     // or replace default theme color
   },
 
   shadows: {
-    md: '1px 1px 3px rgba(0, 0, 0, .25)',
-    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+    md: "1px 1px 3px rgba(0, 0, 0, .25)",
+    xl: "5px 5px 3px rgba(0, 0, 0, .25)",
   },
 
   headings: {
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: "Roboto, sans-serif",
     sizes: {
       h1: { fontSize: rem(36) },
     },
