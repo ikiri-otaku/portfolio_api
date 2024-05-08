215543c75f4448972ad9ca0b1e942e7b1267daae
kucyaman <joinus.ibuki@gmail.com>
2024-05-08 12:50:04 +0900

    :wrench:add テスト用のpostをscaffoldで作成

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_26_132105) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_025308) do
   create_table "organization_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "organization_id", null: false
     t.bigint "user_id", null: false
@@ -27,6 +27,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_132105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["github_username"], name: "index_organizations_on_github_username", unique: true
+  end
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
