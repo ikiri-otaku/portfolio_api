08d6e7cf189dbd759ec16c7ad7c922724766a69a
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-05-11 23:11:49 +0900

    Add: tailwindcssにcustom colorを追加

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,3 +1,5 @@
+import { Secondary } from "@/stories/Button.stories";
+import { PiPlaceholder } from "react-icons/pi";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -12,6 +14,19 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        main: "#632BAD", // NOTE: brand color
+        textBlack: "#0D001E", 
+        textGray: "#6E6C71", 
+        bgWhite: "#F7F7F7", 
+        borderColor: "#EDEDED", 
+        firstRank: "#FFC403", 
+        secondRank: "#C9C9C9", 
+        thirdRank: "#AC6D4D", 
+        placeholder: "#ADB5BD", 
+        inputBorder: "#9B80BF",
+        warning: "#F03E3E",
       },
     },
   },
