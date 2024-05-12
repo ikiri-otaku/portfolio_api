2711ab5f3b3b392ca19dc2085e7f082440badb87
kucyaman <joinus.ibuki@gmail.com>
2024-05-12 17:26:43 +0900

    Nextサーバー側でAPI通信のテスト

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,18 +1,20 @@
-"use client";
 import { MantineProvider } from '@mantine/core';
 import List from './_components/buttons/list';
 import SigninButton from './_components/buttons/signinButton';
 import SignupButton from './_components/buttons/signupButton';
 import Submission from './_components/buttons/submission';
 import theme from './_constants/customTheme';
+import TestPostUI from './_services/testPost';
 
 export default function Home() {
+
   return (
     <MantineProvider theme={theme}>
       <SignupButton />
       <SigninButton />
       <Submission />
       <List />
+      <TestPostUI />
     </MantineProvider>
   );
 }
