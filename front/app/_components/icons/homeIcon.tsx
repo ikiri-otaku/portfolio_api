a636f20edc097f55016e36d3f10331a797872d09
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-05-12 16:33:17 +0900

    Bug: Warning: Invalid DOM property Error 対応

--- front/app/_components/icons/homeIcon.tsx
+++ front/app/_components/icons/homeIcon.tsx
@@ -15,16 +15,16 @@ export const HomeIcon = ({
       <path
         d="M18.3333 8.74997C18.3333 7.74164 17.6583 6.44997 16.8333 5.87497L11.6833 2.26664C10.5166 1.44997 8.64163 1.49164 7.51663 2.36664L3.02496 5.86664C2.27496 6.44997 1.66663 7.69164 1.66663 8.63331V14.8083C1.66663 16.7416 3.24163 18.325 5.17496 18.325H14.825C16.7583 18.325 18.3333 16.7416 18.3333 14.8166V12.2333"
         stroke={stroke}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10 14.9917V12.4917"
         stroke={stroke}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
