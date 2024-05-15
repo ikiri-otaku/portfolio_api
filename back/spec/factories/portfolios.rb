91d6ccd96d6afc26627fc0a4831ec5994cb3b4e5
kucyaman <joinus.ibuki@gmail.com>
2024-05-15 20:34:34 +0900

    テスト初めて書いたよぉん

---
+++ back/spec/factories/portfolios.rb
@@ -0,0 +1,25 @@
+FactoryBot.define do
+  factory :portfolio do
+    sequence :name, 'portfolio_1'
+    sequence :url, 'url_1'
+    introduction { "This is a sample portfolio introduction." }
+    unhealthy_cnt { 0 }
+    latest_health_check_time { Time.now }
+
+    # organizationが必要な場合
+    association :user
+    association :organization
+
+    trait :with_user do
+      after(:create) do |portfolio|
+        portfolio.users << FactoryBot.create(:user)
+      end
+    end
+
+    trait :with_organization do
+      after(:create) do |portfolio|
+        portfolio.organization = FactoryBot.create(:organization)
+      end
+    end
+  end
+end
