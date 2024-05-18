8f8cb01ae7c34dded3eec269d542726d56b3d6bb
kucyaman <joinus.ibuki@gmail.com>
2024-05-19 01:31:31 +0900

    いらなかった

--- back/spec/factories/portfolios.rb
+++ back/spec/factories/portfolios.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :portfolio do
     association :user
-    association :organization
     sequence :name, 'portfolio_1'
     sequence :url, 'url_1'
     introduction { "This is a sample portfolio introduction." }
@@ -11,12 +10,6 @@ FactoryBot.define do
     trait :with_user do
       after(:create) do |portfolio|
         portfolio.users << FactoryBot.create(:user)
-      end
-    end
-
-    trait :with_organization do
-      after(:create) do |portfolio|
-        portfolio.organization = FactoryBot.create(:organization)
       end
     end
   end
