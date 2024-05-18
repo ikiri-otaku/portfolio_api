384a4156647d7f1b78c6f624768cb342eb4f3e1a
kucyaman <joinus.ibuki@gmail.com>
2024-05-19 01:27:56 +0900

    schemaと合わせた

--- back/spec/factories/portfolios.rb
+++ back/spec/factories/portfolios.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :portfolio do
+    association :user
+    association :organization
     sequence :name, 'portfolio_1'
     sequence :url, 'url_1'
     introduction { "This is a sample portfolio introduction." }
     unhealthy_cnt { 0 }
     latest_health_check_time { Time.now }
-
-    association :user
-    association :organization
 
     trait :with_user do
       after(:create) do |portfolio|
