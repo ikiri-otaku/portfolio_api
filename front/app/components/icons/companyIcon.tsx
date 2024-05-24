fd618e2dcbaadefd74b1e7c77964ec49b1fcbe38
yuita-yoshihiko <soup998211@gmail.com>
2024-05-24 10:45:24 +0900

    アイコン追加

---
+++ front/app/components/icons/companyIcon.tsx
@@ -0,0 +1,59 @@
+export const CompanyIcon = ({
+  height = "",
+  width = "",
+  fill = "none",
+  stroke = "#6E6C71",
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 15 15"
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.33348 14.6666H2.72014C1.94681 14.6666 1.31348 14.0466 1.31348 13.2866V3.39324C1.31348 1.64657 2.61348 0.853241 4.20681 1.63324L7.16681 3.08657C7.80681 3.39991 8.33348 4.23324 8.33348 4.93991V14.6666Z"
+        stroke={stroke}
+        strokeWidth="1.0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.6463 10.0401V12.5601C14.6463 14.0001 13.9797 14.6668 12.5397 14.6668H8.33301V6.94678L8.64634 7.01344L11.6463 7.68678L12.9997 7.98678C13.8797 8.18011 14.5997 8.63344 14.6397 9.91344C14.6463 9.95344 14.6463 9.99344 14.6463 10.0401Z"
+        stroke={stroke}
+        strokeWidth="1.0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.66699 6H5.98033"
+        stroke={stroke}
+        strokeWidth="1.0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.66699 8.6665H5.98033"
+        stroke={stroke}
+        strokeWidth="1.0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.6465 7.68652V9.83318C11.6465 10.6599 10.9732 11.3332 10.1465 11.3332C9.31982 11.3332 8.64648 10.6599 8.64648 9.83318V7.01318L11.6465 7.68652Z"
+        stroke={stroke}
+        strokeWidth="1.0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.6398 9.91319C14.5998 10.6999 13.9465 11.3332 13.1465 11.3332C12.3198 11.3332 11.6465 10.6599 11.6465 9.83319V7.68652L12.9998 7.98652C13.8798 8.17986 14.5998 8.63319 14.6398 9.91319Z"
+        stroke={stroke}
+        strokeWidth="1.0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
