b81e774155ff4abaee242ecba4a3af3024596e65
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-05-26 20:43:15 +0900

    Add: ブックマークボタンのコンポーネントを作成

---
+++ front/app/_components/buttons/bookmarkButton.tsx
@@ -0,0 +1,11 @@
+import { BookmarkIcon } from '@/app/_components/icons/bookmarkIcon';
+
+export default function BookmarkButton() {
+  return (
+    <>
+      <button>
+        <BookmarkIcon height="22" width="22" fill="none" stroke="#6E6C71" />
+      </button>
+    </>
+  );
+}
