84a6fcefe5248cc5d889a76e4a2073dfb0e680bb
yuita-yoshihiko <soup998211@gmail.com>
2024-05-29 22:14:08 +0900

    sequenceの書き方おかしかったので修正

--- back/spec/factories/profiles.rb
+++ back/spec/factories/profiles.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :profile do
     association :user
     sequence :x_username, 'x_username_1'
-    sequence :zenn_url, 'https://zenn.dev/example'
-    sequence :qiita_url, 'https://qiita.com/example'
+    sequence :zenn_url, 'https://zenn.dev/example_1'
+    sequence :qiita_url, 'https://qiita.com/example_1'
     sequence :atcoder_username, 'atcoder_username_1'
   end
 end
