80f578f127d760420efc29d293448552209dc25e
yuita-yoshihiko <soup998211@gmail.com>
2024-05-29 22:25:05 +0900

    アソシエーションおかしかったので修正

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :organization_users, dependent: :destroy
   has_many :organizations, through: :organization_users
+  has_one :profile, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   validates :github_username, length: { maximum: 50 }, uniqueness: true
 end
