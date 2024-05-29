c426add2f465013def384dc980147a9c359e8da4
yuita-yoshihiko <soup998211@gmail.com>
2024-05-29 22:32:32 +0900

    テストコードないの重複箇所の切り出し

--- back/spec/models/profile_spec.rb
+++ back/spec/models/profile_spec.rb
@@ -40,37 +40,31 @@ RSpec.describe Profile, type: :model do
 
     describe '重複' do
       let(:another_profile) { FactoryBot.build(:profile) }
-      it 'user_id が重複する場合エラーになること' do
+      before do
         profile.save
-        expect(another_profile.valid?).to be true
+        expect(profile.valid?).to be true
+      end
+      it 'user_id が重複する場合エラーになること' do
         another_profile.user_id = profile.user_id
         expect(another_profile.valid?).to be false
         expect(another_profile.errors[:user_id]).to eq ["has already been taken"]
       end
       it 'x_username が重複する場合エラーになること' do
-        profile.save
-        expect(another_profile.valid?).to be true
         another_profile.x_username = profile.x_username
         expect(another_profile.valid?).to be false
         expect(another_profile.errors[:x_username]).to eq ["has already been taken"]
       end
       it 'zenn_url が重複する場合エラーになること' do
-        profile.save
-        expect(another_profile.valid?).to be true
         another_profile.zenn_url = profile.zenn_url
         expect(another_profile.valid?).to be false
         expect(another_profile.errors[:zenn_url]).to eq ["has already been taken"]
       end
       it 'qiita_url が重複する場合エラーになること' do
-        profile.save
-        expect(another_profile.valid?).to be true
         another_profile.qiita_url = profile.qiita_url
         expect(another_profile.valid?).to be false
         expect(another_profile.errors[:qiita_url]).to eq ["has already been taken"]
       end
       it 'atcoder_username が重複する場合エラーになること' do
-        profile.save
-        expect(another_profile.valid?).to be true
         another_profile.atcoder_username = profile.atcoder_username
         expect(another_profile.valid?).to be false
         expect(another_profile.errors[:atcoder_username]).to eq ["has already been taken"]
