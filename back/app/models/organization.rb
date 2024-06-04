e69d05d92f38d7744054b1497b69cfc31c251d8f
kucyaman <joinus.ibuki@gmail.com>
2024-06-04 23:15:44 +0900

    アソシエーション修正

--- back/app/models/organization.rb
+++ back/app/models/organization.rb
@@ -1,7 +1,18 @@
 class Organization < ApplicationRecord
+  before_destroy :nullify_portfolio_organization_id
+
   has_many :organization_users, dependent: :destroy
   has_many :users, through: :organization_users
-  has_many :portfolios, dependent: :destroy
+  has_many :portfolios, dependent: :nullify
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :github_username, length: { maximum: 50 }, uniqueness: true
+
+  private
+
+  def nullify_portfolio_organization_id
+    portfolios.each do |portfolio|
+      portfolio.update(organization_id: nil)
+    end
+  end
 end
