905e228e18325a5e9271ec34a66a7ea4342c45e9
yuita-yoshihiko <soup998211@gmail.com>
2024-06-05 21:57:23 +0900

    文字数制限

--- back/app/models/profile.rb
+++ back/app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
   validates :user_id, uniqueness: true
   validates :x_username, uniqueness: true, length: { maximum: 50 }
-  validates :zenn_username, uniqueness: true
-  validates :qiita_username, uniqueness: true
+  validates :zenn_username, uniqueness: true, length: { maximum: 50 }
+  validates :qiita_username, uniqueness: true, length: { maximum: 50 }
   validates :atcoder_username, uniqueness: true, length: { maximum: 50 }
 end
