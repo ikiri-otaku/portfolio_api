e7cdb182cca782e161649add53e2e3d9c437e1de
kucyaman <joinus.ibuki@gmail.com>
2024-06-30 15:47:19 +0900

    関連付け変更

--- back/app/models/portfolio.rb
+++ back/app/models/portfolio.rb
@@ -1,8 +1,6 @@
 class Portfolio < ApplicationRecord
-  before_destroy :check_user_dependency
-
-  belongs_to :user, optional: true
-  belongs_to :organization, optional: true
+  belongs_to :user
+  belongs_to :organization
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :url, presence: true,
@@ -10,10 +8,4 @@ class Portfolio < ApplicationRecord
     uniqueness: true,
     format: { with: URI::DEFAULT_PARSER.make_regexp(['http', 'https']) }
   validates :unhealthy_cnt, numericality: { only_integer: true, less_than_or_equal_to: 4, greater_than_or_equal_to: 0 }
-
-  private
-
-  def check_user_dependency
-    throw(:abort) if organization.present?
-  end
 end
