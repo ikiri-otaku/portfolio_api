23b019f40a6945053b3aba0be2c7d13085e5d53b
kucyaman <joinus.ibuki@gmail.com>
2024-07-03 18:34:52 +0900

    rubocop

--- back/spec/models/portfolio_spec.rb
+++ back/spec/models/portfolio_spec.rb
@@ -8,23 +8,23 @@ RSpec.describe Portfolio, type: :model do
       it 'name が空の場合エラーになること' do
         portfolio.name = nil
         expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:name]).to include("can't be blank")
+        expect(portfolio.errors[:name]).to include('を入力してください')
       end
 
       it 'url が空の場合エラーになること' do
         portfolio.url = nil
         expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:url]).to include("can't be blank")
+        expect(portfolio.errors[:url]).to include('を入力してください', 'は不正な値です')
       end
     end
 
     describe 'サイズ' do
       it 'name が50桁を超える場合エラーになること' do
         portfolio.name = 'a' * 50
-        expect(portfolio.valid?).to be true
+        expect(portfolio).to be_valid
         portfolio.name = 'a' * 51
-        expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:name]).to include('is too long (maximum is 50 characters)')
+        portfolio.valid?
+        expect(portfolio.errors[:name]).to include('は50文字以内で入力してください')
       end
 
       it 'url が255桁を超える場合エラーになること' do
@@ -32,7 +32,7 @@ RSpec.describe Portfolio, type: :model do
         expect(portfolio.valid?).to be true
         portfolio.url = "http://#{'a' * 249}"
         expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:url]).to include('is too long (maximum is 255 characters)')
+        expect(portfolio.errors[:url]).to include('は255文字以内で入力してください')
       end
 
       it 'unhealthy_cnt が4を超える場合エラーになること' do
@@ -40,7 +40,7 @@ RSpec.describe Portfolio, type: :model do
         expect(portfolio.valid?).to be true
         portfolio.unhealthy_cnt = 5
         expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:unhealthy_cnt]).to include('must be less than or equal to 4')
+        expect(portfolio.errors[:unhealthy_cnt]).to include('は4以下の値にしてください')
       end
     end
 
@@ -50,49 +50,42 @@ RSpec.describe Portfolio, type: :model do
         expect(portfolio.valid?).to be true
         portfolio.unhealthy_cnt = 'three'
         expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:unhealthy_cnt]).to include('is not a number')
+        expect(portfolio.errors[:unhealthy_cnt]).to include('は数値で入力してください')
       end
 
       it 'unhealthy_cnt が0未満の場合エラーになること' do
         portfolio.unhealthy_cnt = -1
         expect(portfolio.valid?).to be false
-        expect(portfolio.errors[:unhealthy_cnt]).to include('must be greater than or equal to 0')
+        expect(portfolio.errors[:unhealthy_cnt]).to include('は0以上の値にしてください')
       end
     end
 
     describe '重複' do
       let(:another_portfolio) { FactoryBot.build(:portfolio) }
+
       it 'portfolio.url が重複する場合エラーになること' do
         portfolio.save
         expect(another_portfolio.valid?).to be true
         another_portfolio.url = portfolio.url
         expect(another_portfolio.valid?).to be false
-        expect(another_portfolio.errors[:url]).to eq ['has already been taken']
+        expect(another_portfolio.errors[:url]).to eq ['はすでに存在します']
       end
     end
   end
 
   describe 'アソシエーション' do
-    it 'userに属していること' do
-      portfolio = FactoryBot.create(:portfolio)
-      expect(portfolio.user).to be_present
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:organization) { FactoryBot.create(:organization) }
+    let!(:portfolio) { FactoryBot.create(:portfolio, user:, organization:) }
+
+    it 'userの削除時にportfolioをnulに設定' do
+      user.destroy
+      expect(portfolio.reload.user).to be_nil
     end
 
-    it 'organizationが任意であること' do
-      portfolio_without_org = FactoryBot.create(:portfolio)
-      portfolio_with_org = FactoryBot.create(:portfolio, :with_organization)
-      expect(portfolio_without_org.organization).to be_nil
-      expect(portfolio_with_org.organization).to be_present
-    end
-
-    it 'organizationが存在する場合、portfolioを削除できないこと' do
-      portfolio = FactoryBot.create(:portfolio, :with_organization)
-      expect { portfolio.destroy }.to_not(change { Portfolio.count })
-    end
-
-    it 'organizationが存在しない場合、削除が成功すること' do
-      portfolio = FactoryBot.create(:portfolio)
-      expect(portfolio.destroy).to be_truthy
+    it 'organizationの削除時にportfolioをnulに設定' do
+      organization.destroy
+      expect(portfolio.reload.organization).to be_nil
     end
   end
 end
