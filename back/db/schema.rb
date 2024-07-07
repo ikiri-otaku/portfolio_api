bd741d30a6cfb3b8eac2c273f76c3ea2bdf0c2ae
m-shiroi <maho.shiroi@gmail.com>
2024-07-07 20:35:20 +0900

    Pictureモデル

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_053542) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_07_091732) do
   create_table "organization_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "organization_id", null: false
     t.bigint "user_id", null: false
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_053542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["github_username"], name: "index_organizations_on_github_username", unique: true
+  end
+
+  create_table "pictures", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "imageable_type"
+    t.bigint "imageable_id"
+    t.string "object_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["imageable_type", "imageable_id"], name: "index_pictures_on_imageable"
   end
 
   create_table "profiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
--- back/spec/models/profile_spec.rb
+++ back/spec/models/profile_spec.rb
@@ -10,49 +10,49 @@ RSpec.describe Profile, type: :model do
         expect(profile.valid?).to be true
         profile.location = 'a' * 256
         expect(profile.valid?).to be false
-        expect(profile.errors[:location]).to eq ['is too long (maximum is 255 characters)']
+        expect(profile.errors[:location]).to eq ['は255文字以内で入力してください']
       end
       it 'company が255桁を超える場合エラーになること' do
         profile.company = 'a' * 255
         expect(profile.valid?).to be true
         profile.company = 'a' * 256
         expect(profile.valid?).to be false
-        expect(profile.errors[:company]).to eq ['is too long (maximum is 255 characters)']
+        expect(profile.errors[:company]).to eq ['は255文字以内で入力してください']
       end
       it 'work_location が255桁を超える場合エラーになること' do
         profile.work_location = 'a' * 255
         expect(profile.valid?).to be true
         profile.work_location = 'a' * 256
         expect(profile.valid?).to be false
-        expect(profile.errors[:work_location]).to eq ['is too long (maximum is 255 characters)']
+        expect(profile.errors[:work_location]).to eq ['は255文字以内で入力してください']
       end
       it 'x_username が50桁を超える場合エラーになること' do
         profile.x_username = 'a' * 50
         expect(profile.valid?).to be true
         profile.x_username = 'a' * 51
         expect(profile.valid?).to be false
-        expect(profile.errors[:x_username]).to eq ['is too long (maximum is 50 characters)']
+        expect(profile.errors[:x_username]).to eq ['は50文字以内で入力してください']
       end
       it 'zenn_username が50桁を超える場合エラーになること' do
         profile.zenn_username = 'a' * 50
         expect(profile.valid?).to be true
         profile.zenn_username = 'a' * 51
         expect(profile.valid?).to be false
-        expect(profile.errors[:zenn_username]).to eq ['is too long (maximum is 50 characters)']
+        expect(profile.errors[:zenn_username]).to eq ['は50文字以内で入力してください']
       end
       it 'qiita_username が50桁を超える場合エラーになること' do
         profile.qiita_username = 'a' * 50
         expect(profile.valid?).to be true
         profile.qiita_username = 'a' * 51
         expect(profile.valid?).to be false
-        expect(profile.errors[:qiita_username]).to eq ['is too long (maximum is 50 characters)']
+        expect(profile.errors[:qiita_username]).to eq ['は50文字以内で入力してください']
       end
       it 'atcoder_username が50桁を超える場合エラーになること' do
         profile.atcoder_username = 'a' * 50
         expect(profile.valid?).to be true
         profile.atcoder_username = 'a' * 51
         expect(profile.valid?).to be false
-        expect(profile.errors[:atcoder_username]).to eq ['is too long (maximum is 50 characters)']
+        expect(profile.errors[:atcoder_username]).to eq ['は50文字以内で入力してください']
       end
     end
 
@@ -64,7 +64,7 @@ RSpec.describe Profile, type: :model do
         expect(profile.valid?).to be true
         profile.hireable = nil
         expect(profile.valid?).to be false
-        expect(profile.errors[:hireable]).to eq ['is not included in the list']
+        expect(profile.errors[:hireable]).to eq ['は一覧にありません']
       end
     end
 
@@ -77,28 +77,43 @@ RSpec.describe Profile, type: :model do
       it 'user_id が重複する場合エラーになること' do
         another_profile.user_id = profile.user_id
         expect(another_profile.valid?).to be false
-        expect(another_profile.errors[:user_id]).to eq ['has already been taken']
+        expect(another_profile.errors[:user_id]).to eq ['はすでに存在します']
       end
       it 'x_username が重複する場合エラーになること' do
         another_profile.x_username = profile.x_username
         expect(another_profile.valid?).to be false
-        expect(another_profile.errors[:x_username]).to eq ['has already been taken']
+        expect(another_profile.errors[:x_username]).to eq ['はすでに存在します']
       end
       it 'zenn_username が重複する場合エラーになること' do
         another_profile.zenn_username = profile.zenn_username
         expect(another_profile.valid?).to be false
-        expect(another_profile.errors[:zenn_username]).to eq ['has already been taken']
+        expect(another_profile.errors[:zenn_username]).to eq ['はすでに存在します']
       end
       it 'qiita_username が重複する場合エラーになること' do
         another_profile.qiita_username = profile.qiita_username
         expect(another_profile.valid?).to be false
-        expect(another_profile.errors[:qiita_username]).to eq ['has already been taken']
+        expect(another_profile.errors[:qiita_username]).to eq ['はすでに存在します']
       end
       it 'atcoder_username が重複する場合エラーになること' do
         another_profile.atcoder_username = profile.atcoder_username
         expect(another_profile.valid?).to be false
-        expect(another_profile.errors[:atcoder_username]).to eq ['has already been taken']
+        expect(another_profile.errors[:atcoder_username]).to eq ['はすでに存在します']
       end
+    end
+  end
+
+  describe 'アソシエーション' do
+    let(:profile) { FactoryBot.create(:profile, :with_picture) }
+    let(:picture) { profile.pictures.first }
+
+    it '削除すると、関連するデータも削除されること' do
+      profile.save!
+      expect(Picture.where(id: picture.id).count).to eq 1
+
+      profile.destroy!
+
+      expect(Profile.where(id: profile.id).count).to eq 0
+      expect(Picture.where(id: picture.id).count).to eq 0
     end
   end
 end
