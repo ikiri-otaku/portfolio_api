2124c515827d302e352f91cdf944ce8fd6b50571
kucyaman <joinus.ibuki@gmail.com>
2024-08-01 13:56:07 +0900

    like_icon

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -5,6 +5,8 @@ import theme from './_constants/customTheme';
 import AuthButtons from './_layouts/nav/auth_buttons';
 import TestPostUI from './_services/testPost';
 import Header from '@/app/_layouts/header';
+import { LikeOffIcon } from './_components/icons/likeOff';
+import { LikeOnIcon } from './_components/icons/likeOn';
 
 export default function Home() {
   return (
@@ -25,6 +27,8 @@ export default function Home() {
         <p className="mb-[34px] mt-6 text-base font-bold leading-snug text-textGray">
           キャッチコピーキャッチコピーキャッチコピーキャッチコピキャッチコピーキャッチコピー
         </p>
+        <LikeOffIcon width='20px' height='20px'/>
+        <LikeOnIcon width='20px' height='20px'/>
         <Submission />
       </section>
     </MantineProvider>
