b41df7588f0d8fa8800c85ff264e1bb3935559fb
yuita-yoshihiko <soup998211@gmail.com>
2024-08-31 20:26:06 +0900

    テストコード追加

--- back/spec/models/bookmark_spec.rb
+++ back/spec/models/bookmark_spec.rb
@@ -28,4 +28,27 @@ RSpec.describe Bookmark, type: :model do
       expect(another_bookmark.errors[:portfolio_id]).to eq ['はすでに存在します']
     end
   end
+
+  describe 'アソシエーション' do
+    describe '削除' do
+      let!(:user) { FactoryBot.create(:user) }
+      let!(:portfolio) { FactoryBot.create(:portfolio, user:) }
+
+      before do
+        user.bookmarks.create!(portfolio:)
+      end
+
+      it 'user を削除した場合関連するbookmarkが削除されること' do
+        expect { user.destroy! }.to change { Bookmark.count }.from(1).to(0)
+        expect(User.count).to eq 0
+        expect(Portfolio.where(id: portfolio.id).count).to eq 1
+      end
+
+      it 'portfolio を削除した場合関連するbookmarkが削除されること' do
+        expect { portfolio.destroy! }.to change { Bookmark.count }.from(1).to(0)
+        expect(User.where(id: user.id).count).to eq 1
+        expect(Portfolio.count).to eq 0
+      end
+    end
+  end
 end
